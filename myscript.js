74e004e5336051007ced53acc0a9e8ece5259851
Rythem Datta <ritzy192@gmail.com>
2018-07-11 17:20:51 +0530

    Initial Commit

---
+++ myscript.js
@@ -0,0 +1,49 @@
+var count=1;
+var c1=1;
+var c2=1;
+
+function appendlist()
+{
+    var ul=document.getElementById("yourlist");
+    var item=document.getElementById("inputfield");
+    var li=document.createElement("li");
+    // var icon=document.createElement("i");
+    
+    // icon.setAttribute('class',"far fa-trash-alt");
+    // icon.setAttribute('id',count++);
+    // icon.style.color="red";
+    li.setAttribute('id',"list"+count++);
+    li.appendChild(document.createTextNode(item.value));
+    //li.prefix(document.createTextNode(item.value));
+    // li.appendChild(icon);
+    
+    var butdel=document.createElement("button");
+    butdel.innerHTML="Delete";
+    butdel.setAttribute('id',c1++);
+    butdel.style.backgroundColor="black";
+    butdel.style.color="red";
+    butdel.style.fontSize="15px";
+    butdel.style.width="100px";
+
+
+    var butedit=document.createElement("button");
+    butedit.innerHTML="Edit";   
+    butedit.setAttribute('id',c2++);
+    butedit.style.backgroundColor="black";
+    butedit.style.color="yellow";
+    butedit.style.fontSize="15px";
+    butedit.style.width="100px";
+    
+    li.appendChild(butdel);
+    li.appendChild(butedit);
+    
+    li.style.height="100%";
+    li.style.paddingTop="10px";
+    li.style.paddingBottom="10px";
+    li.style.fontSize="20px";
+    li.style.borderStyle="double";
+    li.style.marginBottom="2%";
+
+    document.getElementById("inputfield").value="";
+    ul.appendChild(li);
+}
