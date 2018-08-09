34aee0a1af439db3020516ca3d7c3ebefe59992a
Rythem Datta <ritzy192@gmail.com>
2018-08-09 17:46:37 +0530

    Implemented edit functionality and tried to cover all edge cases

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 function appendlist()
 {
+    count=1;
     var lis=document.createElement("li");
     var inputvalue=document.getElementById("inputfield").value;
     var t=document.createTextNode(inputvalue);
@@ -9,11 +10,17 @@ function appendlist()
         alert("please enter a todo!");
     else
         document.getElementById("yourlist").appendChild(lis);
-    var span = document.createElement("SPAN");
+    var span1 = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
-    span.className="delete";
-    span.appendChild(txt);
-    lis.appendChild(span);
+    var span2 = document.createElement("Span");
+    var txt2 = document.createTextNode("Edit");
+    span1.className="delete";
+    span2.className="edit";        
+    span1.appendChild(txt);
+    span2.appendChild(txt2)
+    lis.appendChild(span2);
+    lis.appendChild(span1);
+
     //click the close button:
     var del = document.getElementsByClassName("delete");
     var i;
@@ -23,5 +30,23 @@ function appendlist()
         var item = this.parentElement;
         item.style.display = "none";
     }
+    }
+    var edt = document.getElementsByClassName("edit");
+    var j;
+    for(j=0;j<edt.length;j++)
+    {
+        edt[j].onclick=function()
+        {
+            count++;
+            if(count%2===0)
+            {
+                var itm=this.parentElement;
+                var temp =itm.innerText;
+                document.getElementById("inputfield").value=temp.slice(0,temp.length-5);
+                itm.style.display="none";
+            }
+            else
+                alert("please edit or add the item first");
+        }
     }
 }
